f7c890102aca21c48e75edcabe90bbe11d7ed955
sophialenharo <145469979+sophialenharo@users.noreply.github.com>
2024-03-19 15:04:42 -0300

    Add files via upload

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 456;
+const numerostring = number ("456a");
+
+// number()
+// string()
+
+console.log(numero + number(numerostring) ) 
+
+// conversão explícita
